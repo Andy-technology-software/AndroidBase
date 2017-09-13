e908125c1d8b6c46ec3afe17933ba2cc697d6e98
xurq <lingnet@lingnet.local>
2017-09-13 17:13:04 +0800

    no message

---
+++ Day2_XMLDemo/app/src/main/java/helloandroid/andy/com/day2_xmldemo/TestCase.java
@@ -0,0 +1,13 @@
+package helloandroid.andy.com.day2_xmldemo;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by lingnet on 2017/9/13.
+ */
+
+public class TestCase extends AndroidTestCase {
+    public void test() {
+
+    }
+}
